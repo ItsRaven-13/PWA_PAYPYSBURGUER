40207e12f96d08a462aec05b3666d763bdbb68a9
ItsRaven-13 <estebanhp2004@gmail.com>
2025-12-02 22:53:58 -0600

    Proyecto PWA

--- sw.js
+++ sw.js
@@ -1,14 +1,25 @@
 const CACHE_NAME = 'paypys-burguer-v1.0.1';
 const urlsToCache = [
-  '/ItsRaven-13.github.io-PWA_PAYPYSBURGUER/',
-  '/ItsRaven-13.github.io-PWA_PAYPYSBURGUER/index.html',
-  '/ItsRaven-13.github.io-PWA_PAYPYSBURGUER/style.css',
-  '/ItsRaven-13.github.io-PWA_PAYPYSBURGUER/app.js',
-  '/ItsRaven-13.github.io-PWA_PAYPYSBURGUER/firebase.js',
-  '/ItsRaven-13.github.io-PWA_PAYPYSBURGUER/login.js',
-  '/ItsRaven-13.github.io-PWA_PAYPYSBURGUER/manifest.json',
-  '/ItsRaven-13.github.io-PWA_PAYPYSBURGUER/assets/img/logo.png'
+  '/PWA_PAYPYSBURGUER/',
+  '/PWA_PAYPYSBURGUER/index.html',
+  '/PWA_PAYPYSBURGUER/manifest.json',
+
+  // CSS
+  '/PWA_PAYPYSBURGUER/assets/css/style.css',
+
+  // JS
+  '/PWA_PAYPYSBURGUER/assets/js/app.js',
+  '/PWA_PAYPYSBURGUER/assets/js/firebase.js',
+  '/PWA_PAYPYSBURGUER/assets/js/login.js',
+  '/PWA_PAYPYSBURGUER/assets/js/registro.js',
+  '/PWA_PAYPYSBURGUER/assets/js/catalogo.js',
+  '/PWA_PAYPYSBURGUER/assets/js/admin.js',
+  '/PWA_PAYPYSBURGUER/assets/js/session.js',
+
+  // IMG
+  '/PWA_PAYPYSBURGUER/assets/img/logo.png'
 ];
+
 
 self.addEventListener("install", event => {
   console.log("Service Worker instalado");
